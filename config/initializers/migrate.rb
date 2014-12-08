886ad288d4b6bb1427f18b19618d40371c3eaeb6
Torsten Ruger <torsten@villataika.fi>
2014-12-08 11:10:56 +0200

    add note

--- config/initializers/migrate.rb
+++
@@ -1,9 +0,0 @@
-# poor mans migration. 
-# Trying to keep the schema clean until version 1
-
-#unless Product.columns_hash["summary"]
-#  ActiveRecord::Base.connection.add_column :products, :summary, :string , :default => ""
-#  end
-#unless Category.columns_hash["summary"]
-#  ActiveRecord::Base.connection.add_column :categories, :summary, :string , :default => ""
-#end
