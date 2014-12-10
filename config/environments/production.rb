898a4342b5e04b3058606016ac82768208920b56
Torsten Ruger <torsten@villataika.fi>
2014-12-10 17:01:56 +0200

    sendmail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,10 +57,11 @@ SalesClerk::Application.configure do
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   config.assets.precompile += %w( sales_clerk.js sales_clerk.css )
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'info@auringostaitaan.fi' , bcc: 'info@auringostaitaan.fi' }
+    
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
   config.i18n.fallbacks = true
