06471a3dc5f6a22ebf38bfece03cb3c6f7c7d977
Torsten Ruger <torsten@villataika.fi>
2014-12-16 10:16:49 +0200

    juggling, history

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,22 @@ Rails.application.routes.draw do
 
  root :to => 'shop#group'
  
-  #shop
+  #group / products
   get 'group/:link' => 'shop#group', :as => :shop_group
   get 'prod/:link' => 'shop#product', :as => :shop_product
-  get 'page/:id' => 'shop#page', :as => :shop_page
+
+  # cart
   match 'cart/add/:id' => 'shop#add', :as => :cart_add , :via => [:get,:post]
   get 'cart/remove/:id' => 'shop#remove', :as => :cart_remove
-  get 'welcome' => 'shop#welcome', :as => :shop_welcome
-  get 'cart/order' => 'shop#order', :as => :shop_order
-  match 'cart/checkout' => 'shop#checkout', :as => :shop_checkout , :via => [:get,:post]
+
+  # checkout + order
+  get 'order' => 'shop#order', :as => :shop_order
+  match 'checkout' => 'shop#checkout', :as => :shop_checkout , :via => [:get,:post]
   get "history" => "shop#history" , :as => :shop_history
+
+  get 'welcome' => 'shop#welcome', :as => :shop_welcome
+  get 'page/:id' => 'shop#page', :as => :shop_page
+
 
   get "/404", :to => "application#error"
 
